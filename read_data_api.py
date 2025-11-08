708c76a7950236c07c64d8c7a7fb2370045caf62
nileshsalunke722 <nileshsalunke722@gmail.com>
2025-11-08 21:45:05 +0530

    testing1

---
+++ read_data_api.py
@@ -0,0 +1,12 @@
+import requests
+import pandas as pd
+import os
+
+token=os.getenv("API_TOKEN")
+print(f"Taken  : {token}")
+
+response=requests.get("https://jsonplaceholder.typicode.com/users")
+data=response.json()
+
+df=pd.DataFrame(data)
+print(df)
